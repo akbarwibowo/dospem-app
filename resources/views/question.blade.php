0b0c2ed444e4fc6a6b1bfda41a6fec7895faebc1
RaihanBaihaqi780 <raihanbaihaqi.2021@student.uny.ac.id>
2023-10-26 09:29:55 +0700

    menghilankan fitur option -baihaqi

---
+++ resources/views/question.blade.php
@@ -0,0 +1,42 @@
+<form action="">
+    <h2>Pertanyaan 1</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan1" id="pertanyaan1-yes" value="yes">
+    <input type="radio" name="pertanyaan1" id="pertanyaan1-no" value="no">
+    <h2>Pertanyaan 2</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan2" id="pertanyaan2-yes" value="yes">
+    <input type="radio" name="pertanyaan2" id="pertanyaan2-no" value="no">
+    <h2>Pertanyaan 3</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan3" id="pertanyaan3-yes" value="yes">
+    <input type="radio" name="pertanyaan3" id="pertanyaan3-no" value="no">
+    <h2>Pertanyaan 4</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan4" id="pertanyaan4-yes" value="yes">
+    <input type="radio" name="pertanyaan4" id="pertanyaan4-no" value="no">
+    <h2>Pertanyaan 5</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan5" id="pertanyaan5-yes" value="yes">
+    <input type="radio" name="pertanyaan5" id="pertanyaan5-no" value="no">
+    <h2>Pertanyaan 6</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan6" id="pertanyaan6-yes" value="yes">
+    <input type="radio" name="pertanyaan6" id="pertanyaan6-no" value="no">
+    <h2>Pertanyaan 7</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan7" id="pertanyaan7-yes" value="yes">
+    <input type="radio" name="pertanyaan7" id="pertanyaan7-no" value="no">
+    <h2>Pertanyaan 8</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan8" id="pertanyaan8-yes" value="yes">
+    <input type="radio" name="pertanyaan8" id="pertanyaan8-no" value="no">
+    <h2>Pertanyaan 9</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan9" id="pertanyaan9-yes" value="yes">
+    <input type="radio" name="pertanyaan9" id="pertanyaan9-no" value="no">
+    <h2>Pertanyaan 10</h2>
+    <input type="text">
+    <input type="radio" name="pertanyaan10" id="pertanyaan10-yes" value="yes">
+    <input type="radio" name="pertanyaan10" id="pertanyaan10-no" value="no">
+</form>
